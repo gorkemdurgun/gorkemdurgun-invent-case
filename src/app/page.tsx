43a35352f838d3d4d9ccee38787a993bb4d2dd20
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-07-29 01:23:49 +0300

    refactor: Disable react/no-unescaped-entities rule in .eslintrc.json and remove container class from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container flex flex-col items-center justify-center gap-2 text-center h-screen">
+    <div className=" flex flex-col items-center justify-center gap-2 text-center h-screen">
       <p>
         Hey, I'm <span className="font-semibold">Görkem Durgun</span>, a front-end developer.
       </p>
